f183c7d543c97191cb5fbb744b592aba0a3f1022
Tulir Asokan <tulir@maunium.net>
2017-07-02 15:32:19 +0300

    Add simple example program

---
+++ cmd/githuuk/githuuk.go
@@ -0,0 +1,62 @@
+// githuuk - A GitHub webhook receiver written in Go.
+// Copyright (C) 2017 Tulir Asokan
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package main
+
+import (
+	"fmt"
+
+	"maunium.net/go/githuuk"
+	flag "maunium.net/go/mauflag"
+)
+
+var port = flag.MakeFull("p", "port", "The port on which to listen for GitHub webhooks.", "80").Uint8()
+var secret = flag.MakeFull("s", "secret", "The GitHub webhook secret.", "").String()
+var wantHelp, _ = flag.MakeHelpFlag()
+
+func main() {
+	flag.SetHelpTitles("githuuk - A very simple command-line utility to test webhooks.", "githuuk [-h] [-p port] [-s secret]")
+	err := flag.Parse()
+	if err != nil {
+		fmt.Println(err)
+		flag.PrintHelp()
+		return
+	} else if *wantHelp {
+		flag.PrintHelp()
+		return
+	}
+
+	server := githuuk.NewServer()
+	server.Port = *port
+	server.Secret = *secret
+	server.AsyncListenAndServe()
+
+	for rawEvent := range server.Events {
+		switch rawEvent.GetType() {
+		case githuuk.EventPush:
+			evt := rawEvent.(*githuuk.PushEvent)
+			fmt.Println(evt.Repository.Owner.Name, evt.Repository.Name, evt.Ref.Name(), evt.HeadCommit.ID)
+		case githuuk.EventPullRequest:
+			evt := rawEvent.(*githuuk.PullRequestEvent)
+			fmt.Println(evt.Repository.Owner.Name, evt.Repository.Name, evt.Action, evt.NumberOfChanges)
+		case githuuk.EventPing:
+			evt := rawEvent.(*githuuk.PingEvent)
+			fmt.Println(evt.Repository.Owner.Name, evt.Repository.Name, evt.Hook.Name, evt.Hook.ID)
+		default:
+			fmt.Println("Unknown event type", rawEvent.GetType())
+		}
+	}
+}
