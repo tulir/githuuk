b24079d8c11ce372e2af5706e752e0511656bda8
Tulir Asokan <tulir@maunium.net>
2017-07-02 15:20:18 +0300

    Rename main file and package

--- event.go
+++ event.go
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-package hookserve
+package githuuk
 
 // EventType represents a GitHub webhook event.
 type EventType string
